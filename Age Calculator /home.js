be138e457960bea42b7957a923cd016b2fd750a4
rishwari <rihwariranjan@gmail.com>
2022-01-28 13:58:41 +0530

    folder struct

---
+++ Age Calculator /home.js
@@ -0,0 +1,13 @@
+console.log("HEllo");
+
+//
+var b = "smoothie";
+console.log(b);
+
+var b = 45;
+console.log(b);
+//var age = prompt("Hello Please Enter Age ");
+//console.log(age);
+//document.getElementById("someText").innerHTML = age;
+var num = 99;
+console.log(num + 8989);
--- home.js
+++
@@ -1,9 +0,0 @@
-console.log("HEllo");
-
-//
-var b = "smoothie";
-console.log(b);
-
-var b = 45;
-console.log(b);
-document.getElementById("someText").innerHTML = "HEEEZ";
