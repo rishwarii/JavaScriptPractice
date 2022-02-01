b72c34e22b18e3f375f03c45bb8bba2c3f18d42d
rishwari <rihwariranjan@gmail.com>
2022-02-01 17:37:06 +0530

    catto generator done

--- All Challenge/static/script.js
+++ All Challenge/static/script.js
@@ -5,11 +5,24 @@ function getAge() {
   var d = new Date();
   var cur = d.getFullYear();
   var daysAlive = (cur - birthyear) * 365;
-  var h1 = document.createElement("h1");
+  var h1 = document.createElement("h1"); //elrment h1
   var textAnswer = document.createTextNode(
     "You are " + daysAlive + " days old"
   );
   h1.setAttribute("id", "daysAlive");
   h1.appendChild(textAnswer);
   document.getElementById("flex-box-results").appendChild(h1);
+}
+
+function resetAge() {
+  document.getElementById("daysAlive").remove();
+}
+
+//Challenege 2 : YOur Catto
+
+function generateCatto() {
+  var img = document.createElement("img");
+  var div = document.getElementById("flex-cat");
+  img.src = "https://thecatapi.com/api/images/get?format=src&type=gif";
+  div.appendChild(img);
 }
